96d4b5972290aca1b20e7b188b20f1828c1f6263
Vladimirshubyn <vladimirshubyn@gmail.com>
2021-02-14 13:39:42 +0700

    Отрисуй меня полностью (часть 1)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,2 @@
 import './mock.js';
+import './picture.js';
---
+++ js/picture.js
@@ -0,0 +1,18 @@
+const pictureTeplate = document.querySelector ('#picture').content;
+const picturesList = document.querySelector('.pictures');
+const sortedPicturesList = [];
+
+const renderMiniatures = function (photo) {
+  const photoMiniature = pictureTeplate.cloneNode(true);
+  photoMiniature.querySelector('img').src = photo.url;
+  photoMiniature.querySelector('.picture__likes').textContent = photo.likes;
+  photoMiniature.querySelector('.picture__comments').textContent = photo.comments.length;
+  return photoMiniature;
+}
+const makeGallery = function () {
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < sortedPicturesList.length; i++) {
+    fragment.appendChild(renderMiniatures(sortedPicturesList[i]));
+  }
+  picturesList.appendChild(fragment);
+};
