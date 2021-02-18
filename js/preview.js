95763c15015243159b5f97821ee110725d938b9b
Vladimirshubyn <vladimirshubyn@gmail.com>
2021-02-18 17:33:29 +0700

    Отрисуй меня полностью (часть 2)

--- js/preview.js
+++ js/preview.js
@@ -1,15 +1,6 @@
 const bigPicture = document.querySelector('.big-picture');
 const bigPictureClose = bigPicture.querySelector('.big-picture__cancel');
 
-window.evt = {
-  ESC_KEYCODE: 27,
-  ENTER_KEYCODE: 13,
-  isKeyEvent: function (evt, action) {
-    if (evt.keyCode === window.evt.ESC_KEYCODE || window.evt.ENTER_KEYCODE) {
-      action();
-    }
-  },
-};
 const closeBigPicture = function () {
   bigPicture.classList.add('hidden');
   document.removeEventListener('keydown', closeKeyHandler);
@@ -21,12 +12,10 @@ const showPreviewHandler = function (elem) {
   elem.addEventListener('click', showBigPicture);
 };
 
-const showBigPicture = function (evt) {
-  evt.preventDefault();
-  const clickedPicture = evt.target.parentNode;
-  bigPicture.querySelector('.big-picture__img').src = clickedPicture.querySelector('img').src;
-  bigPicture.querySelector('.likes-count').textContent = clickedPicture.querySelector('.picture__likes').textContent;
-  bigPicture.querySelector('.comments-count').textContent = clickedPicture.querySelector('.picture__comments').textContent;
+const showBigPicture = function (photoObj) {
+  bigPicture.querySelector('.big-picture__img').src = photoObj.src;
+  bigPicture.querySelector('.likes-count').textContent = photoObj.textContent;
+  bigPicture.querySelector('.comments-count').textContent = photoObj.textContent;
   bigPicture.classList.remove('hidden');
   document.addEventListener('keydown', closeKeyHandler);
   bigPictureClose.focus();
@@ -35,3 +24,5 @@ const showBigPicture = function (evt) {
 document.querySelectorAll('.pictures').forEach(showPreviewHandler);
 bigPictureClose.addEventListener('click', closeBigPicture);
 bigPictureClose.addEventListener('keydown', closeKeyHandler);
+
+export {showBigPicture};
