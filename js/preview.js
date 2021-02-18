ba2cd2df05b070650b5c57249b87c0ad0d70b4fe
Vladimirshubyn <vladimirshubyn@gmail.com>
2021-02-18 19:10:20 +0700

    Отрисуй меня полностью (часть 2)

--- js/preview.js
+++ js/preview.js
@@ -1,28 +1,39 @@
+import {isKeyEscEvent} from './util.js';
+import {generateComments} from './mock.js';
+
 const bigPicture = document.querySelector('.big-picture');
 const bigPictureClose = bigPicture.querySelector('.big-picture__cancel');
+const socialCommentsList = bigPicture.querySelector('.social__comments');
 
 const closeBigPicture = function () {
   bigPicture.classList.add('hidden');
+  document.body.classList.remove('modal-open');
   document.removeEventListener('keydown', closeKeyHandler);
 };
 const closeKeyHandler = function (evt) {
-  window.evt.isKeyEvent(evt, closeBigPicture);
-};
-const showPreviewHandler = function (elem) {
-  elem.addEventListener('click', showBigPicture);
+  isKeyEscEvent(evt, closeBigPicture);
 };
 
 const showBigPicture = function (photoObj) {
-  bigPicture.querySelector('.big-picture__img').src = photoObj.src;
-  bigPicture.querySelector('.likes-count').textContent = photoObj.textContent;
-  bigPicture.querySelector('.comments-count').textContent = photoObj.textContent;
+  bigPicture.querySelector('.social__caption').textContent = photoObj.description;
+  bigPicture.querySelector('.big-picture__img img').src = photoObj.url;
+  bigPicture.querySelector('.likes-count').textContent = photoObj.likes;
+  bigPicture.querySelector('.comments-count').textContent = photoObj.comments.length;
+
+  while (socialCommentsList.firstChild) {
+    socialCommentsList.removeChild(socialCommentsList.firstChild);
+  }
+
+  generateComments();
+
   bigPicture.classList.remove('hidden');
   document.addEventListener('keydown', closeKeyHandler);
   bigPictureClose.focus();
 }
 
-document.querySelectorAll('.pictures').forEach(showPreviewHandler);
 bigPictureClose.addEventListener('click', closeBigPicture);
 bigPictureClose.addEventListener('keydown', closeKeyHandler);
+document.body.classList.add('modal-open');
+
 
 export {showBigPicture};
