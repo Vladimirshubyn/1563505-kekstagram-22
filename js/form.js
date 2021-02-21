32ae480dd4fe364ce064e0eddd6a66cd81e7f4c5
Vladimirshubyn <vladimirshubyn@gmail.com>
2021-02-21 12:26:14 +0700

    Помощь друга (часть 1)

--- js/form.js
+++ js/form.js
@@ -4,6 +4,9 @@ const uploadButton = document.querySelector('#upload-file');
 const uploadForm = document.querySelector('.img-upload__form');
 const editPanel = document.querySelector('.img-upload__overlay');
 const editPanelClose = editPanel.querySelector('#upload-cancel');
+const uploadMessage = uploadForm.querySelector('.img-upload__message--loading');
+const uploadedPicture = editPanel.querySelector('.img-upload__preview > img');
+const uploadErrorMessage = docu.querySelector('.error__message');
 
 const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 
@@ -12,12 +15,10 @@ const successUpload = function () {
   editPanelCloseClick();
 };
 
-const openUploadForm = function () {
-  resize.initialize();
-  document.body.classList.add('modal-open');
-  editPanel.classList.remove('hidden');
-  editPanelClose.addEventListener('click', editPanelCloseClick);
-}
+const errorUpload = function (message) {
+  editPanelCloseClick();
+  uploadErrorMessage.textContent = message;
+};
 
 const editPanelCloseClick = function () {
   uploadForm.reset();
@@ -26,4 +27,38 @@ const editPanelCloseClick = function () {
   document.body.classList.remove('modal-open');
   editPanel.classList.add('hidden');
   editPanelClose.removeEventListener('click', editPanelCloseClick);
+};
+
+const openUploadForm = function () {
+  document.body.classList.add('modal-open');
+  editPanel.classList.remove('hidden');
+};
+
+const uploadFile = function (file, fileTypes, cb) {
+  if (file) {
+    const fileName = file.name.toLowerCase();
+
+    const matches = fileTypes.some((fileType) => fileName.endsWith(fileType));
+
+    if (matches) {
+      const reader = new FileReader();
+
+      reader.addEventListener('load', () => {
+        cb(reader.result);
+      });
+
+      reader.readAsDataURL(file);
+    }
+  }
+};
+
+const initialize = function (evt) {
+  uploadMessage.classList.remove('hidden');
+  const file = evt.target.files[0];
+  uploadFile(file, FILE_TYPES, (readingResult) => {
+    uploadedPicture.src = readingResult;
+
+    openUploadForm();
+    uploadMessage.classList.add('hidden');
+  });
 };
--- js/main.js
+++ js/main.js
@@ -2,3 +2,4 @@ import './picture.js';
 import './preview.js';
 import './resize.js';
 import './form.js';
+import './effects.js';
